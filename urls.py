4aaeef545a6488efe5ec13281997a978782927f7
Douglas Soares de Andrade <contato@douglasandrade.com>
2011-06-16 18:05:58 -0300

    Changing it to use staticfiles to serve the content during development

--- urls.py
+++ urls.py
@@ -1,13 +1,19 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+import settings
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^', include('videos.urls')),
     url(r'^', include(admin.site.urls)),
-
-    # static media
-    url(r'^s/(.*)', 'django.views.static.serve',
-        {'document_root': 'static'},
-        name='static'),
 )
+
+if 'debug_toolbar_htmltidy' in settings.INSTALLED_APPS:
+    urlpatterns += patterns('',
+        url(r'^', include('debug_toolbar_htmltidy.urls')),
+    )
+
+urlpatterns += staticfiles_urlpatterns()
