1a4caed635e652fef6b8988fc67ebda23300a384
Lincoln de Sousa <lincoln@comum.org>
2011-06-21 19:18:30 -0300

    Making it possible to publish rtmp content through our player

--- static/js/player.js
+++ static/js/player.js
@@ -35,9 +35,13 @@ avl.player = (function () {
             var $video = $('<video>');
             var uid = 'player-' + (new Date()).getTime();
             var flv = null;
+            var rtmp = false;
             for (var i = 0; i < this.sources.length; i++) {
                 if (this.sources[i].content_type === 'video/x-flv') {
                     flv = this.sources[i].url;
+                    if (this.sources[i].url.substr(0, 4) === 'rtmp') {
+                        rtmp = true;
+                    }
                 }
 
                 var attrs = {
@@ -47,10 +51,19 @@ avl.player = (function () {
                 $video.append($('<source>').attr(attrs));
             }
 
-            $video.attr({width: this.width, height: this.height});
-            $video.addClass('video-js');
-            $video.append($('<div>').attr('id', uid));
-            $video.appendTo(this.$element);
+            var $playerContainer = $('<div>').attr('id', uid);
+
+            /* There's only one source, a flash one. So, we'll not setup
+             * the video tag :( */
+            if (this.sources.length === 1 && flv) {
+                $playerContainer.appendTo(this.$element);
+                $playerContainer.css({width: this.width, height: this.height});
+            } else {
+                $video.attr({width: this.width, height: this.height});
+                $video.addClass('video-js');
+                $video.append($playerContainer);
+                $video.appendTo(this.$element);
+            }
 
             /* Setting up video js in the just added player. It will not
              * until we add it to the dom structure */
@@ -58,12 +71,38 @@ avl.player = (function () {
 
             /* We need to setup flash too */
             if (flv != null) {
-                $f(uid, "http://releases.flowplayer.org/swf/flowplayer-3.2.7.swf", {
+                var cfg = {
                     clip: {
                         url: flv,
                         autoPlay: true
                     }
-                });
+                };
+
+                /* We also need to support flash streaming */
+                if (rtmp) {
+
+                    /* Handling requests in all styles, including akamai
+                     * ones.
+                     *
+                     * Don't know why they just don't use another
+                     * mimetype once they're not actually providing flv
+                     * content in the first request... */
+                    var result = /^(.+)\/([^\/]+)$/.exec(flv);
+                    var netConnectionUrl = result[1];
+                    var clipUrl = result[0]
+
+                    cfg.clip.url = clipUrl;
+                    cfg.clip.live = true;
+                    cfg.clip.provider = 'influxis';
+                    cfg.plugins = {
+                        influxis: {
+                            url: 'http://releases.flowplayer.org/swf/flowplayer.rtmp-3.2.3.swf',
+                            netConnectionUrl: netConnectionUrl
+                        }
+                    };
+                }
+
+                $f(uid, "http://releases.flowplayer.org/swf/flowplayer-3.2.7.swf", cfg);
             }
             return $video;
         }
